c82630f895212ac64ee0ab24ff6080812f58d50f
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-01-12 12:26:13 +0900

    Add PIL

--- main.py
+++ main.py
@@ -3,13 +3,19 @@
 from flask import Flask
 import numpy as np
 import chainer
+from PIL import Image
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    img = Image.new('L', (64, 64))
+    width, height = img.size
+    print(img)
+    print(width, height)
     return 'hello Falsk'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
